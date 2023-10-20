739eb276f49665f028ad39e63ef47022aa676a26
sicvita <sicvita@butter-rebellion.com>
2023-10-20 14:48:18 -0400

    feat: first website version

---
+++ workers/email-signup/README.md
@@ -0,0 +1,12 @@
+# Quickstart
+
+npm run dev
+
+edit files
+
+optional:
+store secrets in .dev.vars file
+
+wrangler secret put <KEY>
+
+npm run deploy
--- workers/email-signup/src/worker.ts
+++ workers/email-signup/src/worker.ts
@@ -9,7 +9,64 @@
  */
 
 export default {
-  async fetch(request, env, ctx) {
-    return new Response('Hello World!');
-  },
+  async fetch(request, env) {
+
+    const origin = request.headers.get('Origin');
+    const allowedOrigins = ['http://localhost:3000', 'https://l1mine.com'];
+
+    if (request.method === 'OPTIONS' && allowedOrigins.includes(origin)) {
+      return new Response(null, {
+        status: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+      });
+    }
+
+    if (request.method === 'POST') {
+      try {
+        // Parse the request body as JSON to extract the email
+        let requestBody;
+        try {
+          requestBody = await request.json();
+        } catch (error) {
+          return new Response('Invalid or empty JSON data in the request body', { status: 400 });
+        }
+
+        const email = requestBody.email;
+        const user = env.LISTMONK_USER;
+        const pass = env.LISTMONK_PASS;
+
+        // Create the basic authentication header
+        const authHeader = 'Basic ' + btoa(user + ':' + pass);
+
+        // Prepare the request to Listmonk API
+        const response = await fetch('https://listmonk.l1mine.com/api/subscribers', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': authHeader,
+          },
+          body: JSON.stringify({
+            email,
+            name: email,
+            status: "enabled"
+          }),
+        });
+
+        if (response.ok) {
+          return new Response('Successfully joined the waitlist', { status: 200 });
+        } else {
+          return new Response('Something went wrong. Please try again later', { status: 500 });
+        }
+      } catch (error) {
+        console.error(error);
+        return new Response('Error processing the request', { status: 500 });
+      }
+    } else {
+      return new Response('Invalid request method', { status: 400 });
+    }
+  }
 };
