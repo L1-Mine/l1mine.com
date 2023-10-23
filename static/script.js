7de4a33ef76005fd3108f0bc85171062282453a4
sicvita <sicvita@butter-rebellion.com>
2023-10-23 07:14:21 -0400

    fix: google lighthouse score

--- static/script.js
+++ static/script.js
@@ -22,83 +22,104 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-const canvasSize = 300;
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(canvasSize, canvasSize);
+const renderWebGLCanvas = () => {
+  const canvasSize = 300;
+  let scene = new THREE.Scene();
+  let camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+  let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(canvasSize, canvasSize);
 
-document.getElementById("webgl").appendChild(renderer.domElement);
+  document.getElementById("webgl").appendChild(renderer.domElement);
 
-function onWindowResize() {
-  camera.aspect = 1;
-  camera.updateProjectionMatrix();
-  renderer.setSize(
-    canvasSize * window.devicePixelRatio,
-    canvasSize * window.devicePixelRatio,
-    false
-  );
-}
-
-window.addEventListener("resize", onWindowResize);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
-directionalLight.position.set(5, 1, 5);
-
-const directionalLight2 = new THREE.DirectionalLight(0xffffff, 10);
-directionalLight2.position.set(-5, 1, -5);
-scene.add(directionalLight);
-scene.add(directionalLight2);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableZoom = false;
-
-let loader = new GLTFLoader();
-const textureLoader = new THREE.TextureLoader();
-
-const baseColorTexture = textureLoader.load(
-  "static/bitcoin/textures/Material.001_baseColor.png"
-);
-const metallicRoughnessTexture = textureLoader.load(
-  "static/bitcoin/textures/Material.001_metallicRoughness.png"
-);
-
-const pbrMaterial = new THREE.MeshStandardMaterial({
-  map: baseColorTexture,
-  metalnessMap: metallicRoughnessTexture,
-  roughnessMap: metallicRoughnessTexture,
-  metalness: 1,
-  roughness: 1,
-});
-
-let coin;
-loader.load("static/bitcoin/scene.gltf", function (gltf) {
-  coin = gltf.scene;
-  const size = 100;
-  coin.scale.set(size, size, size);
-  coin.traverse((node) => {
-    if (node.isMesh) {
-      node.material = pbrMaterial;
-      node.material.needsUpdate = true;
-    }
-  });
-  scene.add(coin);
-});
-
-camera.position.z = 15;
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  if (coin) {
-    coin.rotation.y += 0.005;
+  function onWindowResize() {
+    camera.aspect = 1;
+    camera.updateProjectionMatrix();
+    renderer.setSize(
+      canvasSize * window.devicePixelRatio,
+      canvasSize * window.devicePixelRatio,
+      false
+    );
   }
 
-  renderer.render(scene, camera);
+  window.addEventListener("resize", onWindowResize);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+  directionalLight.position.set(5, 1, 5);
+
+  const directionalLight2 = new THREE.DirectionalLight(0xffffff, 10);
+  directionalLight2.position.set(-5, 1, -5);
+  scene.add(directionalLight);
+  scene.add(directionalLight2);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableZoom = false;
+
+  let loader = new GLTFLoader();
+  const textureLoader = new THREE.TextureLoader();
+
+  const baseColorTexture = textureLoader.load(
+    "static/bitcoin/textures/Material.001_baseColor.png"
+  );
+  const metallicRoughnessTexture = textureLoader.load(
+    "static/bitcoin/textures/Material.001_metallicRoughness.png"
+  );
+
+  const pbrMaterial = new THREE.MeshStandardMaterial({
+    map: baseColorTexture,
+    metalnessMap: metallicRoughnessTexture,
+    roughnessMap: metallicRoughnessTexture,
+    metalness: 1,
+    roughness: 1,
+  });
+
+  let coin;
+  loader.load("static/bitcoin/scene.gltf", function (gltf) {
+    coin = gltf.scene;
+    const size = 100;
+    coin.scale.set(size, size, size);
+    coin.traverse((node) => {
+      if (node.isMesh) {
+        node.material = pbrMaterial;
+        node.material.needsUpdate = true;
+      }
+    });
+    scene.add(coin);
+  });
+
+  camera.position.z = 15;
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    if (coin) {
+      coin.rotation.y += 0.005;
+    }
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
+};
+
+function loadVideoBackground() {
+  if (window.innerWidth > 1025) {
+    const video = document.createElement("video");
+    video.id = "backgroundVideo";
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    video.playsInline = true;
+    video.poster = "/static/fallback.jpg";
+    video.src = "/static/background.mp4";
+    document.body.appendChild(video);
+  }
 }
 
-animate();
+window.addEventListener("load", () => {
+  loadVideoBackground();
+  renderWebGLCanvas();
+});
